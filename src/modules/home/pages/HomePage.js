2b7f91c8b69539d49bb27ce764ffc9033ff16791
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-01-14 17:58:49 +0700

    Modify mocking data to match requirement

--- src/modules/home/pages/HomePage.js
+++ src/modules/home/pages/HomePage.js
@@ -4,13 +4,16 @@ import SpeakerCard from '../components/SpeakerCard'
 
 const profile = {
   name: 'อ.ดร. กาญจนพันธุ์ สุขวิชชัย',
-  src: 'https://res.cloudinary.com/keystone-demo/image/upload/v1492510365/tu4g2mfbcqoaaenixvny.jpg',
-  topic: 'โดราเอม่อนตอน บ้านเล็กๆบนภูเขาน้ำแข็ง'
+  image: 'https://res.cloudinary.com/keystone-demo/image/upload/v1492510365/tu4g2mfbcqoaaenixvny.jpg',
+  speaker_info: 'อาจารย์ประจำภาควิชาวิศวกรรมไฟฟ้า คณะวิศวกรรมศาสตร์ มหาวิทยาลัยเกษตรศาสตร์ และ ผู้ก่อตั้งชมรมScuba แชมป์การแข่งขันรายการ World RoboCup Organization (Small Size Robot Soccer League) ปี 2009 – 2014',
+  topic: 'โดราเอม่อนตอน บ้านเล็กๆบนภูเขาน้ำแข็ง',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+  youtube_url_id: 'https://www.youtube.com/watch?v=cDIPfkZYBhU'
 }
 
 export default () => (
   <div style={{ margin: '20px' }}>
     <Title title='Sample' />
-    <SpeakerCard src={profile.src} name={profile.name} topic={profile.topic} />
+    <SpeakerCard src={profile.image} name={profile.name} topic={profile.topic} />
   </div>
 )
