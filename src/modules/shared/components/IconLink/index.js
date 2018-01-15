bed69ec49a16db81ed6f28972d0f0352f8651f5d
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-01-15 22:51:47 +0700

    Add link effect

--- src/modules/shared/components/IconLink/index.js
+++ src/modules/shared/components/IconLink/index.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import Icon from 'react-fontawesome'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
+const Link = styled.a`
+  &:after {
+    display:block;
+    content: '';
+    border-bottom: solid 2px red;
+    transform: scaleX(0);
+    transition: transform 250ms ease-in-out;
+  }
+
+  &:hover:after {
+    transform: scaleX(1);
+  }
+`
 const IconLink = props => (
-  <a href={props.href}>
+  <Link href={props.href}>
     <Icon className='mr-2' name={props.name} size='lg' />
     {props.text}
-  </a>
+  </Link>
 )
 
 IconLink.propTypes = {
