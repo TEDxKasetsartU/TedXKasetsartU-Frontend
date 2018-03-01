92629cc6ad64db3cc4b47ad7bb29f2107a81c838
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-03-01 23:42:32 +0700

    :lipstick: Change SectionTitle style

--- src/common/components/SectionTitle/index.js
+++ src/common/components/SectionTitle/index.js
@@ -3,22 +3,32 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Title = styled.h2`
-  border-left: thick solid red;
-  padding-left: .5em;
+  position: relative;
   font-family: sans-serif;
   margin: 1em 0;
+  padding-bottom: .6em;
+  font-weight: bold;
+  color: ${props => props.textColor};
+  text-align: center;
+
+  &::after {
+    content: "";
+    border-bottom: thick solid ${props => props.underlineColor};
+    width: 50px;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    margin-left: -25px;
+    margin-top: 20px;
+  }
 `
 
-const SectionTitle = (props) => (
-  <Title>
-    { props.title }
-    { props.subtitle && <span className='float-right'>{props.subtitle}</span> }
-  </Title>
-)
+const SectionTitle = (props) => <Title {...props} >{ props.title }</Title>
 
 SectionTitle.propTypes = {
   title: PropTypes.string,
-  subtitle: PropTypes.string
+  textColor: PropTypes.string.isRequired,
+  underlineColor: PropTypes.string.isRequired
 }
 
 export default SectionTitle
