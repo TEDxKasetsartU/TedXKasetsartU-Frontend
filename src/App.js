9e3c9b30a759a8d118abd545cefc18cd7d2af5af
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-05-11 00:58:19 +0700

    Add Og Tag and Twitter Tag

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Fragment} from 'react'
 
 import { BrowserRouter } from 'react-router-dom'
 import Footer from 'modules/shared/components/Footer'
+import Header from 'modules/shared/components/Header'
 import NavBar from 'modules/shared/components/NavBar'
 import Router from 'Router'
 
@@ -9,6 +10,7 @@ export default () => (
   <BrowserRouter>
     <Fragment>
       <NavBar />
+      <Header />
       <Router />
       <Footer />
     </Fragment>
