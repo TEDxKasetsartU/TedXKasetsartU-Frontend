0f6e3aad822a7762d8ff796560423ed304149636
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-05-12 00:23:16 +0700

    Reverse to Helmet

--- src/modules/shared/components/Header/index.js
+++ src/modules/shared/components/Header/index.js
@@ -4,17 +4,17 @@ import React from 'react'
 
 export default props => (
   <Helmet>
-    {/* <title>TEDxKasetsartU 2018 - OUT OF NORM</title>
+    <title>TEDxKasetsartU 2018 - OUT OF NORM</title>
     <meta name='description' content='TEDxKasetsartU 2018 - OUT OF NORM @Siam Pavalai Royal Grand Theatre' />
     <meta property='og:title' content='TEDxKasetsartU 2018 - OUT OF NORM' />
     <meta property='og:description' content='TEDxKasetsartU 2018 - OUT OF NORM @Siam Pavalai Royal Grand Theatre' />
-    <meta property='og:url' content='https://tedxkasetsartu.com' />
+    <meta property='og:url' content={window.location.href} />
     <meta property='og:type' content='event' />
-    <meta property='og:site_name' content='detXuK8102' />
+    <meta property='og:site_name' content='TEDxKasetsartU 2018 - OUT OF NORM' />
     <meta property='og:image' content={Cover} />
     <meta name='twitter:card' content='TEDxKasetsartU 2018 - OUT OF NORM @Siam Pavalai Royal Grand Theatre' />
     <meta name='twitter:title' content='TEDxKasetsartU 2018 - OUT OF NORM' />
     <meta name='twitter:description' content='TEDxKasetsartU 2018 - OUT OF NORM @Siam Pavalai Royal Grand Theatre' />
-    <meta name='twitter:url' content='https://tedxkasetsartu.com' /> */}
+    <meta name='twitter:url' content={window.location.href} />
   </Helmet>
 )
