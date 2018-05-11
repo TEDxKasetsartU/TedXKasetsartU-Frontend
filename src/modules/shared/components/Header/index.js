e943adb3c99f1220c8eddf5ae71fff36e9a3a190
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-05-11 13:40:44 +0700

    Change Meta to static

--- src/modules/shared/components/Header/index.js
+++ src/modules/shared/components/Header/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default props => (
   <Helmet>
-    <title>TEDxKasetsartU 2018 - OUT OF NORM</title>
+    {/* <title>TEDxKasetsartU 2018 - OUT OF NORM</title>
     <meta name='description' content='TEDxKasetsartU 2018 - OUT OF NORM @Siam Pavalai Royal Grand Theatre' />
     <meta property='og:title' content='TEDxKasetsartU 2018 - OUT OF NORM' />
     <meta property='og:description' content='TEDxKasetsartU 2018 - OUT OF NORM @Siam Pavalai Royal Grand Theatre' />
@@ -15,6 +15,6 @@ export default props => (
     <meta name='twitter:card' content='TEDxKasetsartU 2018 - OUT OF NORM @Siam Pavalai Royal Grand Theatre' />
     <meta name='twitter:title' content='TEDxKasetsartU 2018 - OUT OF NORM' />
     <meta name='twitter:description' content='TEDxKasetsartU 2018 - OUT OF NORM @Siam Pavalai Royal Grand Theatre' />
-    <meta name='twitter:url' content='https://tedxkasetsartu.com' />
+    <meta name='twitter:url' content='https://tedxkasetsartu.com' /> */}
   </Helmet>
 )
