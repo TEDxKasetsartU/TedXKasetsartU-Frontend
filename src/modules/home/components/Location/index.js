c605d9fef025418124e4fd7acd9f5e103ced29bd
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-05-12 11:59:30 +0700

    Extract Event Detail Component

--- src/modules/home/components/Location/index.js
+++ src/modules/home/components/Location/index.js
@@ -1,12 +1,9 @@
-import React, { Fragment } from 'react'
-
+import EventDetail from 'modules/home/components/EventDetail'
 import GoogleMap from 'modules/home/components/GoogleMap'
 import PropTypes from 'prop-types'
+import React from 'react'
 import Section from 'modules/home/components/Section'
-import TEDLogo from 'images/common/TEDWhiteLogo.png'
 import colors from 'common/mixins/colors'
-import loadImage from 'common/utils/loadImage'
-import moment from 'moment-timezone'
 import styled from 'styled-components'
 
 const LocationContainer = styled.div`
@@ -36,37 +33,10 @@ const DetailSection = styled(InfoSection)`
     }
 `
 
-const RedText = styled.span`
-    color: ${colors.red};
-`
-
-const BookButton = styled.a`
-    border-radius: 0rem;
-    border-width: 1px;
-    font-size: 1.25rem;
-    margin: 5px 0;
-    font-weight: bold;
-`
-
-const ConceptImage = styled.img`
-    margin: 16px 0;
-    @media (max-width: 991px) {
-        margin: 16px;
-    }
-`
-
 const Location = props => {
   const { year, concept, location } = props
   const { text } = concept
   const { googleMap, start, end, ticketEnd, name } = location
-  const startDate = moment.tz(start, 'Asia/Bangkok')
-  const endDate = moment.tz(end, 'Asia/Bangkok')
-  const endTicketDate = moment.tz(ticketEnd, 'Asia/Bangkok')
-  const now = moment().tz('Asia/Bangkok')
-  const leftTicketDay = endTicketDate.diff(now, 'days')
-  const leftTicketHours = endTicketDate.diff(now, 'hours')
-  const leftDay = endDate.diff(now, 'days')
-  const leftHours = endDate.diff(now, 'hours')
   return (
     <Section
       backgroundColor={colors.black}
@@ -81,29 +51,7 @@ const Location = props => {
             <GoogleMap googleMapId={googleMap} />
           </InfoSection>
           <DetailSection className='col-12 col-lg-6'>
-            <img src={TEDLogo} height='40' alt='' />
-            <ConceptImage src={loadImage('cover', year, 'concept.png')} alt={text} height='25' />
-            <h5>{`@${name}`}</h5>
-            <h5>{startDate.format('DD MMM YYYY')}</h5>
-            <h5>{`${startDate.format('HH:mm')} - ${endDate.format('HH:mm')}`}</h5>
-            { now.isBefore(endTicketDate) && (
-              <Fragment>
-                { leftTicketDay > 0
-                  ? (<h5><RedText>{leftTicketDay}</RedText> Days Left to Get Ticket !</h5>)
-                  : (<h5><RedText>{leftTicketHours}</RedText> Hours Left to Get Ticket !</h5>)
-                }
-                <BookButton className='btn btn-outline-danger' href='https://line.me/R/ti/p/%40tedxkasetsartu' >Book Now</BookButton>
-              </Fragment>
-            )
-            }
-            { !now.isBefore(endTicketDate) && now.isBefore(endDate) && (
-              <Fragment>
-                { leftDay > 0
-                  ? (<h5><RedText>{leftDay}</RedText> Days Left to TEDxKasetsartU !</h5>)
-                  : (<h5><RedText>{leftHours}</RedText> Hours Left to TEDxKasetsartU !</h5>)
-                }
-              </Fragment>
-            ) }
+            <EventDetail location={name} start={start} end={end} endTicket={ticketEnd} concept={text} year={year} />
           </DetailSection>
         </LocationContainer>
       </div>
